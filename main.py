9ee15bb60e5f726618be26bfa06206b1986e67df
BoSott <bosse.sottmann@stud.uni-heidelberg.de>
2021-08-03 11:30:58 +0200

    improve readability of the main function

--- main.py
+++ main.py
@@ -8,33 +8,66 @@ import numpy
 import matplotlib.pyplot as plt
 import random
 
-# defining the number of steps
-n = 100000
+def usr_input():
+    # command line stuff
+    pass
 
-# creating two array for containing x and y coordinate
-# of size equals to the number of size and filled up with 0's
-x = numpy.zeros(n)
-y = numpy.zeros(n)
 
 # filling the coordinates with random variables
-for i in range(1, n):
-    val = random.randint(1, 4)
-    if val == 1:
-        x[i] = x[i - 1] + 1
-        y[i] = y[i - 1]
-    elif val == 2:
-        x[i] = x[i - 1] - 1
-        y[i] = y[i - 1]
-    elif val == 3:
-        x[i] = x[i - 1]
-        y[i] = y[i - 1] + 1
-    else:
-        x[i] = x[i - 1]
-        y[i] = y[i - 1] - 1
+def normal_walker(n, x, y):
+    for pos in range(1, n):
+        direction = random.randint(1, 4)
+        # go east
+        if direction == 1:
+            x[pos] = x[pos - 1] + 1
+            y[pos] = y[pos - 1]
+        # go west
+        elif direction == 2:
+            x[pos] = x[pos - 1] - 1
+            y[pos] = y[pos - 1]
+        # go north
+        elif direction == 3:
+            x[pos] = x[pos - 1]
+            y[pos] = y[pos - 1] + 1
+        # go south
+        else:
+            x[pos] = x[pos - 1]
+            y[pos] = y[pos - 1] - 1
+
+def fast_walker():
+    pass
+
+def add_landscape():
+    pass
+
+def some_other_wlaker():
+    pass
 
 
 # plotting the walk
-plt.title("Random Walk ($n = " + str(n) + "$ steps)")
-plt.plot(x, y)
-plt.savefig("./rand_walk_{}.png".format(n))
-plt.show()
+def plot_walk(n, x, y):
+    plt.title("Random Walk ($n = " + str(n) + "$ steps)")
+    plt.plot(x, y)
+    plt.savefig("./rand_walk_{}.png".format(n))
+    plt.show()
+
+
+def main():
+    #TODO document in the README
+
+    # defining the number of steps
+    n = 10000
+
+    # creating two array for containing x and y coordinate
+    # of size equals to the number of size and filled up with 0's
+    x = numpy.zeros(n)
+    y = numpy.zeros(n)
+
+    # multiple walkers
+    normal_walker(n, x, y)
+
+    plot_walk(n, x, y)
+
+
+if __name__ == "__main__":
+    main()
