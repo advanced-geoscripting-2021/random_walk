2e68fafd7dcfd4e0bfbbea32f4a4b8f63a2209ec
Christina Ludwig <christina.ludwig@uni-heidelberg.de>
2021-08-03 20:49:56 +0200

    add randomwalk.py

---
+++ randomwalk.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""__description__
+"""
+
+__author__ = "Christina Ludwig, GIScience Research Group, Heidelberg University"
+__email__ = "christina.ludwig@uni-heidelberg.de"
+
+class Walker():
+    """Random Walker"""
+
+    def __init__(self):
